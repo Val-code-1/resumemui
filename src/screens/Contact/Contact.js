ea17d44e322e9f9193fc3d2d906cc08ee4936033
Val-code-1 <reit.ben@gmail.com>
2022-02-08 23:42:29 -0600

    Made the layout of the Contact page

--- src/screens/Contact/Contact.js
+++ src/screens/Contact/Contact.js
@@ -1,5 +1,43 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  Button,
+  Grid,
+  TextareaAutosize,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 export default function Contact() {
-  return <Box>This is the Contact page</Box>;
+  return (
+    <Box>
+      <Typography variant="h2" color="primary">
+        Send me an E-mail
+      </Typography>
+      <br />
+      <Grid container spacing={2} columns={1} direction="column">
+        <Grid item>
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            type={"email"}
+          />
+        </Grid>
+        <Grid item>
+          <TextareaAutosize
+            aria-label="minimum height"
+            minRows={5}
+            placeholder="Send me a message"
+            style={{ width: 400 }}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained">Send</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
