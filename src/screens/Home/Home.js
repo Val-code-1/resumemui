6b5fea2ce26b27070fc571435c23e93d3e1faf5c
Val-code-1 <reit.ben@gmail.com>
2022-02-11 03:43:44 -0600

    Messing around with gsap

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,11 +1,17 @@
+import { useRef, useEffect } from "react";
 import { Box } from "@mui/system";
+import { gsap } from "gsap";
 import "./Home.css";
 import img from "../../assets/Bojack.png";
 
 const Home = () => {
+  const imgRef = useRef();
+  useEffect(() => {
+    gsap.from(imgRef.current, { duration: 5.5, ease: "power4.out", y: 500 });
+  });
   return (
     <Box id="background">
-      <img src={img} id="img" alt="Bojack Horseman" />
+      <img src={img} id="img" ref={imgRef} alt="Bojack Horseman" />
     </Box>
   );
 };
