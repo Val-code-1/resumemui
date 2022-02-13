731d2868cd58ab49c7e0408400dd761a0a5dfd7b
Val-code-1 <reit.ben@gmail.com>
2022-02-13 14:20:56 -0600

    Added some more gsap to portfolio page

--- src/screens/Portfolio/Portfolio.js
+++ src/screens/Portfolio/Portfolio.js
@@ -1,9 +1,16 @@
+import { useRef, useEffect } from "react";
 import { Box, Grid } from "@mui/material";
 import Diviner from "./Projects/Diviner";
+import gsap from "gsap";
 
 export default function Portfolio() {
+  const boxRef = useRef();
+
+  useEffect(() => {
+    gsap.from(boxRef.current, { duration: 0.5, ease: "power4.out", scale: 0.5 });
+  });
   return (
-    <Box>
+    <Box ref={boxRef}>
       <Grid container spacing={8} columns={2} rows={2} justifyContent="center">
         <Grid item m={12}>
           <Diviner />
